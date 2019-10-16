fba294a45d0a80a6f2916f38e83756f1945571d8
Jimmy Lin <jimmydlin@gmail.com>
2019-10-16 16:25:48 -0700

    calculator easy and hard

---
+++ hardcal.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+    var num1=""
+    var num2=""
+    var oper=""
+
+    $(".container").on("click", "button", function(e){
+
+       var val=$(this).html()
+      
+       if(val=== '='){
+           if(oper==="+") {
+               $("#result").html(Number(num1) + Number(num2))
+           } else if(oper==="-") {
+            $("#result").html(Number(num1) - Number(num2))
+        } else if (oper==="X") {
+            $("#result").html(Number(num1) * Number(num2))
+        } else if(oper==="/") {
+            $("#result").html(Number(num1) / Number(num2))
+        }
+
+       } else if (val=== "+" || val=== "-" || val=== "X" || val==="/") {
+
+        oper = val
+
+       } else if (val==="C") {
+           num1=""
+           num2=''
+           oper=''
+
+       } else {
+           if(oper==="") {
+            num1 += val
+           } else {
+            num2 += val
+           }
+       }
+
+     
+       console.log("num1:" + num1)
+       console.log("oper:" + oper)
+       console.log("num2:" + num2)
+    })
+
+})
